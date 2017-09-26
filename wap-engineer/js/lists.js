58e202d4738f2e124d5f4a2583f6da5b60e8a21e
guchuan <1973231806@qq.com>
2017-09-26 17:59:38 +0800

    新加工程师WAP

--- wap-engineer/js/lists.js
+++ wap-engineer/js/lists.js
@@ -1,3 +1,29 @@
-$(function(){
-    console.log(window);
+
+$("#fakerstar").on("change",function () {
+    if($(this).val()){
+        $("#fakerstarplar").hide();
+    }else {
+        $("#fakerstarplar").show();
+    }
 });
+$("#fakerstarbor").on("change",function () {
+    if($(this).val()){
+        $("#fakerstarborplar").hide();
+    }else {
+        $("#fakerstarborplar").show();
+    }
+});
+
+$('.hot').on("touchmove scroll",function () {
+    if ($(this)[0].clientHeight + $(this)[0].scrollTop >=  $(this)[0].scrollHeight && $("#gifForlist").css("display")==="none") {
+        $("#gifForlist").show();
+        setTimeout(function () {
+            $("#alists").append("<li><h1>"+Math.random()+"</h1></li>");
+            $("#gifForlist").hide();
+        },3000);
+    }
+    if($("#alists").height()<$(".hot").height()-$(".hot").css("top").split("px")[0]){
+        /*当初始记录没有滚动条，走此逻辑*/
+    }
+});
+
